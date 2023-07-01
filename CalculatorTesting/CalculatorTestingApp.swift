3545e91ef7539185ef336fa7957b50f4b08be4d0
Syed Raza <syed.raza011@gmail.com>
2023-07-01 11:59:53 -0400

    Initial Commit

---
+++ CalculatorTesting/CalculatorTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorTestingApp.swift
+//  CalculatorTesting
+//
+//  Created by Syed Raza on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
